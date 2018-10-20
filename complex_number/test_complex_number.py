f8daaf79de0bb14ef88b1a762e4e6ce7adee133a
mohira <shukatsudaison@gmail.com>
2018-10-20 13:32:23 +0900

    mohira: 整数2を渡すと純虚数2iが文字列として返されるテストがREDな状態にした

---
+++ complex_number/test_complex_number.py
@@ -0,0 +1,19 @@
+import unittest
+
+
+class ComplexNumber:
+    def create_純虚数_from整数(self, integer_number: int) -> str:
+        pass
+
+
+class TestImaginaryNumber(unittest.TestCase):
+    def test_整数2を渡すと純虚数2iが文字列として返される(self):
+        complex_number = ComplexNumber()
+
+        actual = complex_number.create_純虚数_from整数(integer_number=2)
+
+        self.assertEqual("2i", actual)
+
+
+if __name__ == "__main__":
+    unittest.main()
