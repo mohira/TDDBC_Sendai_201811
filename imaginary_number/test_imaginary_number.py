efb8144d27644ec2885df59c73cb8b486f8fe70b
mohira <shukatsudaison@gmail.com>
2018-10-20 13:10:36 +0900

    mohira: Pycharmプロジェクト作成と落ちるテストを実装

---
+++ imaginary_number/test_imaginary_number.py
@@ -0,0 +1,10 @@
+import unittest
+
+
+class TestImaginaryNumber(unittest.TestCase):
+    def test_落ちるテスト(self):
+        self.fail("このテストは落ちるはずでっせ！")
+
+
+if __name__ == "__main__":
+    unittest.main()
