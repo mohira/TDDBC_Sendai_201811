0ddcd393ca4e35a83007506b22dd86f320c2523c
mohira <shukatsudaison@gmail.com>
2018-10-20 16:22:35 +0900

    mohira/kawanishi: 共役な純虚数を返すテストと実装を追加

--- complex_number.py
+++ complex_number.py
@@ -6,7 +6,17 @@ class ComplexNumber:
         if integer_number == -1:
             return "-i"
 
-        return f"{integer_number}i"
+        return "{}i".format(integer_number)
 
     def 同一ですか(self, 純虚数その1, 純虚数その2):
         return 純虚数その1 == 純虚数その2
+
+    def create_共役な純虚数_from純虚数(self, 純虚数: str):
+        if 純虚数 == "i":
+            return "-i"
+        if 純虚数 == "-i":
+            return "i"
+
+        虚部 = int(純虚数[:-1])
+
+        return "{}i".format(-1 * 虚部)
--- test_complex_number.py
+++ test_complex_number.py
@@ -34,6 +34,21 @@ class TestImaginaryNumber(unittest.TestCase):
 
         self.assertFalse(self.complex_number.同一ですか(純虚数その1, 純虚数その2))
 
+    def test_純虚数を渡すと共役な純虚数が返される_正常系(self):
+        純虚数3i = self.complex_number.create_純虚数_from整数(integer_number=3)
+
+        self.assertEqual("-3i", self.complex_number.create_共役な純虚数_from純虚数(純虚数3i))
+
+    def test_純虚数を渡すと共役な純虚数が返される_準正常系_その1(self):
+        純虚数i = self.complex_number.create_純虚数_from整数(integer_number=1)
+
+        self.assertEqual("-i", self.complex_number.create_共役な純虚数_from純虚数(純虚数i))
+
+    def test_純虚数を渡すと共役な純虚数が返される_準正常系_その2(self):
+        純虚数マイナスi = self.complex_number.create_純虚数_from整数(integer_number=-1)
+
+        self.assertEqual("i", self.complex_number.create_共役な純虚数_from純虚数(純虚数マイナスi))
+
 
 if __name__ == "__main__":
     unittest.main()
