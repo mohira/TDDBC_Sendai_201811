5d4e9fb0fe38da862350f146f9883f59c974fd25
mohira <shukatsudaison@gmail.com>
2018-10-21 00:49:04 +0900

    実部という概念を追加

--- complex_number.py
+++ complex_number.py
@@ -7,12 +7,7 @@ class 虚数単位(Enum):
 
 
 class 虚部条件Error(Exception):
-    # 数学の定義において虚部は実数だが、現状は整数のみを虚部の条件としている
-    pass
-
-
-class 実部条件Error(Exception):
-    # 数学の定義において実部は実数だが、現状は整数のみを虚部の条件としている
+    # 数学の定義において虚部は実数だが、現状は0以外の整数のみを虚部の条件としている
     pass
 
 
@@ -40,17 +35,30 @@ class PurelyImaginaryNumber:
         return PurelyImaginaryNumber(-1 * self.虚部)
 
 
-class ImaginaryNumber:
-    def __init__(self, 実部: int, 虚部: int):
+class 実部条件Error(Exception):
+    # 数学の定義において実部は実数だが、現状は0以外の整数を虚部の条件としている
+    pass
+
+
+class RealNumber:
+    def __init__(self, 実部: int):
         if 実部 == 0 or (not isinstance(実部, int)):
             raise 実部条件Error(f"{実部}は0以外の整数である必要があります。")
 
         self.実部 = 実部
+
+    def to_str(self):
+        return str(self.実部)
+
+
+class ImaginaryNumber:
+    def __init__(self, 実部: int, 虚部: int):
+        self.実部 = RealNumber(実部)
         self.純虚数 = PurelyImaginaryNumber(虚部)
 
     def to_str(self) -> str:
         if self.純虚数.虚部 > 0:
-            return f"{self.実部} + {self.純虚数.to_str()}"
+            return f"{self.実部.to_str()} + {self.純虚数.to_str()}"
 
         if self.純虚数.虚部 < 0:
-            return f"{self.実部} - {self.純虚数.to_共役().to_str()}"
+            return f"{self.実部.to_str()} - {self.純虚数.to_共役().to_str()}"
