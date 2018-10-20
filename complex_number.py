8a047aef97f6024ae6fd0dc8991c47f4d714d2b3
mohira <shukatsudaison@gmail.com>
2018-10-20 23:54:17 +0900

    虚数単位の概念をEnumとして導入した

--- complex_number.py
+++ complex_number.py
@@ -1,3 +1,11 @@
+from enum import Enum
+
+
+class 虚数単位(Enum):
+    plus = "i"
+    minus = "-i"
+
+
 class PurelyImaginaryNumber:
     def __init__(self, num: int = None):
         self.虚部 = num
@@ -8,12 +16,12 @@ class PurelyImaginaryNumber:
 
     def value_as_str(self) -> str:
         if self.虚部 == 1:
-            return "i"
+            return 虚数単位.plus.value
 
         if self.虚部 == -1:
-            return "-i"
+            return 虚数単位.minus.value
 
-        return f"{self.虚部}i"
+        return f"{self.虚部}{虚数単位.plus.value}"
 
     def to_共役(self):
         return PurelyImaginaryNumber(-1 * self.虚部)
