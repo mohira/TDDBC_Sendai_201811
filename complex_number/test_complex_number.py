3075b3c3fd0ea0f4a62aa4f45f59ae94fd2637b7
mohira <shukatsudaison@gmail.com>
2018-10-20 14:05:45 +0900

    mohira: 正常系の実装

--- complex_number/test_complex_number.py
+++ complex_number/test_complex_number.py
@@ -3,25 +3,27 @@ import unittest
 
 class ComplexNumber:
     def create_純虚数_from整数(self, integer_number: int) -> str:
-        return "2i"
+        return f"{integer_number}i"
 
 
 class TestImaginaryNumber(unittest.TestCase):
-    def test_整数2を渡すと純虚数2iが文字列として返される(self):
-        complex_number = ComplexNumber()
+    def setUp(self):
+        self.complex_number = ComplexNumber()
 
-        actual = complex_number.create_純虚数_from整数(integer_number=2)
+    def test_整数2を渡すと純虚数2iが文字列として返される(self):
+        actual = self.complex_number.create_純虚数_from整数(integer_number=2)
 
         self.assertEqual("2i", actual)
 
     def test_整数3を渡すと純虚数3iが文字列として返される(self):
-        complex_number = ComplexNumber()
-
-        actual = complex_number.create_純虚数_from整数(integer_number=3)
+        actual = self.complex_number.create_純虚数_from整数(integer_number=3)
 
         self.assertEqual("3i", actual)
 
+    def test_整数マイナス4を渡すと純虚数マイナス4iが文字列として返される(self):
+        actual = self.complex_number.create_純虚数_from整数(integer_number=4)
 
+        self.assertEqual("4i", actual)
 
 
 if __name__ == "__main__":
