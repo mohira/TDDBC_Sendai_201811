d699b4139247c0d1b0ebb7eb86b1b7cfe129186f
mohira <shukatsudaison@gmail.com>
2018-10-20 16:55:00 +0900

    mohira: テストコードのリファクタリングをした

--- test_complex_number.py
+++ test_complex_number.py
@@ -8,19 +8,13 @@ class TestComplexNumber(unittest.TestCase):
         self.complex_number = ComplexNumber()
 
     def test_整数を渡すと純虚数が文字列として返される_正常系(self):
-        actual = self.complex_number.create_純虚数_from整数(integer_number=2)
-
-        self.assertEqual("2i", actual)
+        self.assertEqual("2i", self.complex_number.create_純虚数_from整数(integer_number=2))
 
     def test_整数を渡すと純虚数が文字列として返される_準正常系_その1(self):
-        actual = self.complex_number.create_純虚数_from整数(integer_number=1)
-
-        self.assertEqual("i", actual)
+        self.assertEqual("i", self.complex_number.create_純虚数_from整数(integer_number=1))
 
     def test_整数を渡すと純虚数が文字列として返される_準正常系_その2(self):
-        actual = self.complex_number.create_純虚数_from整数(integer_number=-1)
-
-        self.assertEqual("-i", actual)
+        self.assertEqual("-i", self.complex_number.create_純虚数_from整数(integer_number=-1))
 
     def test_2つの純虚数の同一性の判定_同一である(self):
         純虚数その1 = self.complex_number.create_純虚数_from整数(integer_number=3)
